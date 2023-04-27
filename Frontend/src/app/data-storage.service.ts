1880830c7b3e7afaca7af3c7cb4514e8c71fa49b
BerenyiPeti <berenyipeti01@gmail.com>
2023-04-27 11:17:53 +0200

    adatok megjelennek az oldalon adatbázisból

--- Frontend/src/app/data-storage.service.ts
+++ Frontend/src/app/data-storage.service.ts
@@ -1,15 +1,41 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
+import { map, tap } from "rxjs";
+import { DolgozatokService } from "./dolgozatok-form/dolgozatok.service";
+import { Szakdoga } from "./dolgozatok-form/interfaces";
 @Injectable({
     providedIn: 'root',
 })
 
 export class DataStorageService {
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient, private dolServ: DolgozatokService) { }
 
     fetchRows() {
-        return this.http.get('https://localhost:7258/szakdogak')
+        this.http.get<Szakdoga[]>(URL + '/szakdogak').pipe(
+            map((rows) => {
+                const data = rows.map((row) => {
+                    const record: Szakdoga = {
+                        id: row.id,
+                        szakdoga_nev: row.szakdoga_nev,
+                        tagokneve: row.tagokneve,
+                        githublink: row.githublink,
+                        oldallink: row.oldallink
+                    };
+                    return { ...record };
+                });
+                return data;
+            }),
+            tap({
+                next: (data) => this.dolServ.setAdatok(data.slice()),
+                error: (error) => console.log(error),
+            })
+        )
+            .subscribe();
     }
+
+    /* fetchRows() {
+        return this.http.get('https://localhost:7258/szakdogak')
+    } */
 
 
 }
--- Frontend/src/app/dolgozatok-form/dolgozatok-form.component.ts
+++ Frontend/src/app/dolgozatok-form/dolgozatok-form.component.ts
@@ -5,6 +5,7 @@ import { DolgozatokService } from './dolgozatok.service';
 import { Subscription } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { DataStorageService } from '../data-storage.service';
+import { NgForm } from '@angular/forms';
 
 
 @Component({
@@ -19,14 +20,22 @@ export class DolgozatokFormComponent implements OnInit, OnDestroy {
   adatok: Szakdoga[]
   dataFetchSub: Subscription
   dataChangedSub: Subscription
+
+  cim: string = ''
+  keszitok: string = ''
+  elerhetoseg: string = ''
+  github: string = ''
+  modositas: boolean = false
+
   tesztadatok: Szakdoga[] = [{ id: 1, githublink: 'github1', oldallink: 'oldallink1', szakdoga_nev: 'cim1', tagokneve: 'tagok1' }, { id: 2, githublink: 'github2', oldallink: 'oldallink2', szakdoga_nev: 'cim2', tagokneve: 'tagok2' }]
 
   ngOnInit(): void {
-    //this.adatok = this.tesztadatok
-    this.dataFetchSub = this.dsService.fetchRows().subscribe((data)=>{
+    this.adatok = this.tesztadatok
+    /* this.dataFetchSub = this.dsService.fetchRows().subscribe((data) => {
       console.log(data);
-      
-    })
+
+    }) */
+    this.dsService.fetchRows()
 
     this.dataChangedSub = this.dolServ.dataChanged.subscribe((data: Szakdoga[]) => {
       this.adatok = data
@@ -41,4 +50,25 @@ export class DolgozatokFormComponent implements OnInit, OnDestroy {
     this.dataChangedSub.unsubscribe()
   }
 
+
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+
+  }
+
+  onModositas(sz: Szakdoga) {
+    this.modositas = true
+    this.cim = sz.szakdoga_nev
+    this.keszitok = sz.tagokneve
+    this.elerhetoseg = sz.oldallink
+    this.github = sz.githublink
+  }
+
+  onMegse() {
+    this.modositas = false
+    this.cim = ''
+    this.keszitok = ''
+    this.elerhetoseg = ''
+    this.github = ''
+  }
 }
